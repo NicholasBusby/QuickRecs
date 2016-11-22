f3d7109568561350732b9e2c52b8f83c5535871d
Nicholas Busby <Nicholas.Busby@Live.com>
2016-11-21 20:29:47 -0800

    tastekid now gets called and gets deserialized.

---
+++ QuickRecs/QuickRecs/QuickRecs/Service/Dependancy.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Practices.Unity;
+using ModernHttpClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickRecs.Service
+{
+    public class Dependancy
+    {
+        public static void Initialize()
+        {
+            App.DependancyContainer = new UnityContainer();
+            App.DependancyContainer.RegisterType<HttpMessageHandler>(new InjectionFactory(x => new NativeMessageHandler()));
+            App.DependancyContainer.RegisterType<TasteKid>();
+        }
+    }
+}
